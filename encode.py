303ce2cf85b6f3bca77552e80930b9346b2bcf40
Saurabh Mathur <saurabhmathur96@gmail.com>
2019-12-05 17:26:45 -0500

    Create encode.py

---
+++ encode.py
@@ -0,0 +1,22 @@
+''' encode.py: takes the absolute value of STFT of mp3 files
+
+  Usage:
+    $ python3 encode.py mp3_dir/ output_dir/
+'''
+import numpy as np
+import librosa
+import tqdm
+import argparse
+import os
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('mp3_dir')
+parser.add_argument('output_dir')
+args = parser.parse_args()
+
+os.makedirs(args.output_dir, exist_ok=True)
+for mp3 in tqdm.tqdm(os.listdir(args.mp3_dir)):
+  signal, rate = librosa.load(os.path.join(base, mp3))
+  encoded = np.abs(librosa.stft(mp3, n_fft=1024, hop_length=512)).T
+  np.save(os.path.join(args.output_dir, f'{mp3}.npy'), encoded)
