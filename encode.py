b42eb3ca3ee8f5f10ab7d3f0b5588ffbb6bdec38
Saurabh Mathur <saurabhmathur96@gmail.com>
2019-12-05 18:53:09 -0500

    Update encode.py

--- encode.py
+++ encode.py
@@ -17,6 +17,6 @@ args = parser.parse_args()
 
 os.makedirs(args.output_dir, exist_ok=True)
 for mp3 in tqdm.tqdm(os.listdir(args.mp3_dir)):
-  signal, rate = librosa.load(os.path.join(base, mp3))
+  signal, rate = librosa.load(os.path.join(args.mp3_dir, mp3))
   encoded = np.abs(librosa.stft(mp3, n_fft=1024, hop_length=512)).T
   np.save(os.path.join(args.output_dir, f'{mp3}.npy'), encoded)
